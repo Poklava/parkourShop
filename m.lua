8fc0cd77507c8e3ac551c446d3a66d6c2c08bec6
Poklava <66903746+Poklava@users.noreply.github.com>
2022-02-15 19:22:37 +0300

    Update m.lua

--- m.lua
+++ m.lua
@@ -9,47 +9,47 @@ local textAreaIds = {
 }
 local items = {
     [1] = {
-        {img = "17ee0479ad8.png", x = 195, y = 120, scale = 1, id = 142}, 
-        {img = "17ee0483361.png", x = 275 + 80, y = 110, scale = 1, id = 139}, 
-        {img = "17ee048cb5f.png", x = 325 + 80 * 2, y = 115, scale = 1, id = 109}, 
-        {img = "17ee0491764.png", x = 190, y = 150 + 60, scale = 1, id = 126}, 
-        {img = "17ee0496361.png", x = 275 + 80, y = 160 + 60, scale = 1, id = 140}, 
-        {img = "17ee04a945f.png", x = 340 + 80 * 2, y = 175 + 60, scale = 1, id = 107}, 
-        {img = "17ee0520164.png", x = 210, y = 130, scale = 1, id = 122}
+        {img = "17ee0479ad8.png", x = 135, y = 120, scale = 1, id = 142}, 
+        {img = "17ee0483361.png", x = 215 + 80, y = 110, scale = 1, id = 139}, 
+        {img = "17ee048cb5f.png", x = 265 + 80 * 2, y = 115, scale = 1, id = 109}, 
+        {img = "17ee0491764.png", x = 130, y = 150 + 60, scale = 1, id = 126}, 
+        {img = "17ee0496361.png", x = 215 + 80, y = 160 + 60, scale = 1, id = 140}, 
+        {img = "17ee04a945f.png", x = 280 + 80 * 2, y = 175 + 60, scale = 1, id = 107}, 
+        {img = "17ee0520164.png", x = 150, y = 130, scale = 1, id = 122}
     },
     [2] = {
-        {img = "17ee049af62.png", x = 205, y = 120, scale = 0.6, id = 212}, 
-        {img = "17ee049fbb2.png", x = 285 + 80, y = 120, scale = 0.6, id = 222}, 
-        {img = "17ee04ae061.png", x = 350 + 80 * 2, y = 125, scale = 0.6, id = 224}, 
-        {img = "17ee04b2c62.png", x = 217, y = 165 + 60, scale = 0.6, id = 219}, 
-        {img = "17ee04b785f.png", x = 275 + 80, y = 157 + 60, scale = 0.6, id = 230}, 
-        {img = "17ee04bc460.png", x = 345 + 80 * 2, y = 173 + 60, scale = 0.6, id = 241}, 
-        {img = "17ee04c5c61.png", x = 210, y = 115, scale = 0.6, id = 233}, 
-        {img = "17ee04d8c60.png", x = 280 + 80, y = 117, scale = 0.6, id = 244}, 
-        {img = "17ee04dd863.png", x = 355 + 80 * 2, y = 120, scale = 0.6, id = 246}, 
-        {img = "17ee04e7061.png", x = 360, y = 175 + 60, scale = 0.6, id = 240}, 
-        {img = "17ee0503961.png", x = 120 + 80, y = 170 + 60, scale = 0.6, id = 248}, 
-        {img = "17ee0524d64.png", x = 345 + 80 * 2, y = 170 + 60, scale = 0.6, id = 243}, 
-        {img = "17ee0487f63.png", x = 360, y = 113, scale = 0.6, id = 235}, 
-        {img = "17ee047e75f.png", x = 133 + 80, y = 127, scale = 0.6, id = 226}
+        {img = "17ee049af62.png", x = 145, y = 120, scale = 0.6, id = 212}, 
+        {img = "17ee049fbb2.png", x = 225 + 80, y = 120, scale = 0.6, id = 222}, 
+        {img = "17ee04ae061.png", x = 290 + 80 * 2, y = 125, scale = 0.6, id = 224}, 
+        {img = "17ee04b2c62.png", x = 157, y = 165 + 60, scale = 0.6, id = 219}, 
+        {img = "17ee04b785f.png", x = 215 + 80, y = 157 + 60, scale = 0.6, id = 230}, 
+        {img = "17ee04bc460.png", x = 285 + 80 * 2, y = 173 + 60, scale = 0.6, id = 241}, 
+        {img = "17ee04c5c61.png", x = 150, y = 115, scale = 0.6, id = 233}, 
+        {img = "17ee04d8c60.png", x = 220 + 80, y = 117, scale = 0.6, id = 244}, 
+        {img = "17ee04dd863.png", x = 295 + 80 * 2, y = 120, scale = 0.6, id = 246}, 
+        {img = "17ee04e7061.png", x = 300, y = 175 + 60, scale = 0.6, id = 240}, 
+        {img = "17ee0503961.png", x = 60 + 80, y = 170 + 60, scale = 0.6, id = 248}, 
+        {img = "17ee0524d64.png", x = 285 + 80 * 2, y = 170 + 60, scale = 0.6, id = 243}, 
+        {img = "17ee0487f63.png", x = 300, y = 113, scale = 0.6, id = 235}, 
+        {img = "17ee047e75f.png", x = 73 + 80, y = 127, scale = 0.6, id = 226}
     },
     [3] = {
-        {img = "17ee04c1063.png", x = 208, y = 110, scale = 0.8, id = 2818},
-        {img = "17ee04ca860.png", x = 275 + 80, y = 120, scale = 1, id = 2877}, 
-        {img = "17ee04cf463.png", x = 340 + 80 * 2, y = 113, scale = 1, id = 2825}, 
-        {img = "17ee04e2460.png", x = 205, y = 160 + 60, scale = 1, id = 2801}, 
-        {img = "17ee04ebc63.png", x = 260 + 80, y = 155 + 60, scale = 1, id = 2819}, 
-        {img = "17ee04f0861.png", x = 335 + 80 * 2, y = 153 + 60, scale = 1, id = 2832}, 
-        {img = "17ee04f5461.png", x = 195, y = 115, scale = 1, id = 2840}, 
-        {img = "17ee04fa16d.png", x = 257 + 80, y = 110, scale = 1, id = 2829}, 
-        {img = "17ee04fed61.png", x = 335  + 80 * 2, y = 100, scale = 1, id = 2841}, 
-        {img = "17ee050d162.png", x = 169, y = 145 + 60, scale = 1, id = 2837}
+        {img = "17ee04c1063.png", x = 148, y = 110, scale = 0.8, id = 2818},
+        {img = "17ee04ca860.png", x = 215 + 80, y = 120, scale = 1, id = 2877}, 
+        {img = "17ee04cf463.png", x = 280 + 80 * 2, y = 113, scale = 1, id = 2825}, 
+        {img = "17ee04e2460.png", x = 145, y = 160 + 60, scale = 1, id = 2801}, 
+        {img = "17ee04ebc63.png", x = 200 + 80, y = 155 + 60, scale = 1, id = 2819}, 
+        {img = "17ee04f0861.png", x = 275 + 80 * 2, y = 153 + 60, scale = 1, id = 2832}, 
+        {img = "17ee04f5461.png", x = 135, y = 115, scale = 1, id = 2840}, 
+        {img = "17ee04fa16d.png", x = 197 + 80, y = 110, scale = 1, id = 2829}, 
+        {img = "17ee04fed61.png", x = 275  + 80 * 2, y = 100, scale = 1, id = 2841}, 
+        {img = "17ee050d162.png", x = 109, y = 145 + 60, scale = 1, id = 2837}
     },
     [4] = {
-        {img = "17ee0508560.png", x = 490, y = 135, scale = 0.7, id = 703}, 
-        {img = "17ee051b563.png", x = 268 + 80, y = 140, scale = 0.7, id = 707}, 
-        {img = "17ee0529961.png", x = 40  + 80 * 2, y = 140, scale = 0.7, id = 702}, 
-        {img = "17ee0511d62.png", x = 195, y = 170 + 65, scale = 0.650, id = 705}
+        {img = "17ee0508560.png", x = 430, y = 135, scale = 0.7, id = 703}, 
+        {img = "17ee051b563.png", x = 208 + 80, y = 140, scale = 0.7, id = 707}, 
+        {img = "17ee0529961.png", x = -20  + 80 * 2, y = 140, scale = 0.7, id = 702}, 
+        {img = "17ee0511d62.png", x = 135, y = 170 + 65, scale = 0.650, id = 705}
     }
 }
 
@@ -110,16 +110,16 @@ end
 
 displayShop = function(name)
     players[name].shopImg = tfm.exec.addImage("17201a440b4.png", ":0", 400, 212.5, name, 1.25, 1.25, 0, 1, 0.5, 0.5)
+    players[name].shopBackgorundImg = tfm.exec.addImage("166dc37c641.png", ":0", 400, 212.5, name, 0.79, 0.88, 0, 0.55, 0.5, 0.5)
     players[name].shopCloseButton = tfm.exec.addImage("1717a7b2c60.png", ":0", 705, 46, name, 1, 1, 0, 1, 0.5, 0.5)
     ui.addTextArea(textAreaIds.shopCloseButton, "<a href='event:shopCloseButton'>  </a>", name, 695, 43, 13, 13,
         0xf00000, 0xf00000, 0, true)
-
-    for i, j in next, {["shopTab1"] = {"Küçük Kutu", 165}, ["shopTab2"] = {"Büyük Kutu", 290}, ["shopTab3"] = {"Balon", 440}, ["shopTab4"] = {"Trambolin", 540}} do
+        
+    local tabAreaX = {145, 270, 420, 520}
+    for i, j in next, {["shopTab1"] = {"Küçük Kutu", tabAreaX[1]}, ["shopTab2"] = {"Büyük Kutu", tabAreaX[2]}, ["shopTab3"] = {"Balon", tabAreaX[3]}, ["shopTab4"] = {"Trambolin", tabAreaX[4]}} do
         ui.addTextArea(textAreaIds[i], "<a href='event:" .. i .. "'>    " .. j[1] .. "</a>", name, j[2], 80, 100, 20,
         0xf00000, 0xf00000, 0, true)
     end
-
-    local tabAreaX = {165, 290, 440, 540}
     for i = 1, #items do
         players[name].tempImgs[i] = tfm.exec.addImage(players[name].shopTab == i and "17272e2e9bd.png" or "17272e306ca.png", ":1", tabAreaX[i], 82, name, 1, 1, 0, 1, 0, 0)
     end
@@ -129,8 +129,8 @@ displayShop = function(name)
         players[name].tempImgs[#players[name].tempImgs + 1] = tfm.exec.addImage(items[players[name].shopTab][i].img, "&2", items[players[name].shopTab][i].x, items[players[name].shopTab][i].y, name, items[players[name].shopTab][i].scale, items[players[name].shopTab][i].scale, 0, 1, 0, 0, 0)
         textAreaIds[#textAreaIds + 1] = i + 6
         textAreaIds[#textAreaIds + 1] = (i + 6) * 2
-        ui.addTextArea(i + 6, "", name, textAreaX, textAreaY, 79, 85, 0x253942, 0x142529, 1, true)
-        ui.addTextArea((i + 6) * 2 , "<p align='center'><a href='event:shopButton" .. i .. "'>" .. (players[name].inventory[items[players[name].shopTab][i].id] and "Kullan" or "BILGI") .. "</a></p>", name, textAreaX, textAreaY + 70, 80, 20, 0x253942, 0x142529, 1, true)    
+        ui.addTextArea(i + 6, "", name, textAreaX, textAreaY, 79, 85, 0x253942, 0x142529, 0.5, true)
+        ui.addTextArea((i + 6) * 2 , "<p align='center'><a href='event:shopButton" .. i .. "'>" .. (players[name].inventory[items[players[name].shopTab][i].id] and "Kullan" or "BILGI") .. "</a></p>", name, textAreaX, textAreaY + 70, 80, 20, 0x253942, 0x142529, 0.5, true)    
         textAreaX = textAreaX + 150
         if textAreaX == 600 then
             textAreaX = 150
@@ -149,7 +149,7 @@ displayShop = function(name)
 end
 
 removeShop = function (name)
-    for _, i in next, {'shopImg', 'shopCloseButton'} do
+    for _, i in next, {'shopImg', 'shopCloseButton', 'shopBackgorundImg'} do
         tfm.exec.removeImage(players[name][i])
     end
     for i in next, textAreaIds do
